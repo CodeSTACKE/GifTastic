79cf6175904cf8ee522815c67c838860a34ac3f3
Jyoti Das <bootcampatut@gmail.com>
2019-06-10 23:28:59 -0500

    working extra features of the hw

--- assets/Ajax/gifTastic.js
+++ assets/Ajax/gifTastic.js
@@ -3,8 +3,10 @@ var foodDrink = ["Burger", "Kabab", "Cake", "Pancakes", "Frenchfries", "Margarit
 LIMIT = 5;
 var DATA_ITEM;
 var gifArray=[];
-var META;
-var metaArray=[];
+var META1=null;
+var META2=null;
+
+
 // dynamically create buttons
 function renderButtons() {
   for (var i = 0; i < foodDrink.length; i++) {
@@ -19,43 +21,56 @@ function renderButtons() {
   }
 }
 //function for opening the Giphy API connection
-function openconnection(DATA_ITEM ,LIMIT,metaArray) {
+function openconnection(DATA_ITEM ,LIMIT) {
   
-  var giphyURL = [];
+  var p;
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + DATA_ITEM + "&api_key=eb540ZBUIedT5TTDkoMEJbo9EC5pNd3P&limit=" + LIMIT + "";
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    console.log(response)
+    console.log(response);
+    
+    console.log("Insde open connection"+META1,META2);
     var divRow = $("<div>");
     divRow.addClass("row");
     console.log(queryURL);
-    console.log("MetaURL "+metaArray);
-    for(var i=0;i<metaArray-length;i++)
-    {
-      console.log("INside for meta");
-      debugger;
-      if(metaArray[i]==="Title")
-      {
-        var title=metaArray[i];
-        console.log("Inside titlr"+title);
-      }
-      else if(metaArray[i]==="Tags")
-      {
-        var tags=metaArray[i];
-        console.log("inside tags"+tags);
-      }
-    }
-    for (var i = 0; i < response.data.length; i++) {
-      
+     for (var i = 0; i < response.data.length; i++) {
       var divColumn = $("<div>");
       divColumn.addClass("column");
       var card = $("<div>");
       card.addClass("card");
       var showImg = $("<Img>");
-      var p = $("<p>").text("Rating :" +response.data[i].rating.toUpperCase());
-      var animatedSrc = response.data[i].images.fixed_height.url;
+       p = $("<p>").text("Rating :" +response.data[i].rating.toUpperCase());
+        if(META1===null && META2===null)
+       console.log("Meta is null");
+        else if(META1==="title" &&  META2==="type"){
+         var title = $("<p>").text("Title :" +response.data[i].title.toUpperCase());
+         var type= $("<p>").text("Type :" +response.data[i].type.toUpperCase());
+         p.append(title);
+         p.append(type);
+         console.log("Inside if condition 2"+title);
+         console.log("Inside if condition 2"+type);
+         
+
+       }
+       else if(META1==="title" && META2===null)
+       {
+         var title = $("<p>").text("Title :" +response.data[i].title.toUpperCase());
+         console.log("Inside if condition 2"+title);
+         p.append(title);
+         console.log("Inside if condition 2"+title);
+         
+         
+       }
+       else if(META1==="null" && META2==="type")
+       {
+         var type= $("<p>").text("Type :" +response.data[i].type.toUpperCase());
+         p.append(type);
+         console.log("Inside if condition 2"+type);
+         
+       }  
+       var animatedSrc = response.data[i].images.fixed_height.url;
       console.log(animatedSrc);
       var stillSrc = response.data[i].images.fixed_height_still.url;
       console.log(stillSrc);
@@ -74,7 +89,11 @@ function openconnection(DATA_ITEM ,LIMIT,metaArray) {
     }
 
   });
-}
+  console.log("ISIDE"+LIMIT);
+  //intializin the meta variable to null 
+  meta2=null;
+  meta1=null;
+  }
 
 
 
@@ -91,6 +110,7 @@ $(document).on("click", "#add-food", function () {
   renderButtons();
   $("#food-input").val(" ");
 });
+
 //   displays the image when the button clicked.
 $(document).on("click", ".showbutton", function () {
   DATA_ITEM = $(this).attr("data-item");
@@ -99,27 +119,73 @@ $(document).on("click", ".showbutton", function () {
   $(".container").empty();
   openconnection(DATA_ITEM,LIMIT);
 });
+
 // Displaying additional gifs on user request.
 $(document).on("click", "#add-gif", function () {
   event.preventDefault();
-  LIMIT = $("input[name='gif']:checked").val();
-  console.log(LIMIT);
+  LIMIT=0;
+  console.log("LIMIT 0:"+LIMIT);
+   console.log(($("input[name='gif']:checked").val()));
+  if(($("input[name='gif']:checked").val())==="5")
+  {
+    LIMIT=5;
+    
+    $("#5").prop("checked",false);
+    $("#10").prop("checked",false);
+    console.log("LIMIT 5:"+LIMIT);
+  }
+  else 
+  if(($("input[name='gif']:checked").val())==="10")
+  {
+    LIMIT=10;
+    $("#10").prop("checked",false);
+    $("#5").prop("checked",false);
+    console.log("LIMIT 10:"+LIMIT);
+  }
+  else{
+    alert("please select one ");
+  }
   openconnection(DATA_ITEM,LIMIT);
+  LIMIT=5;
+  
 
 //dipslaying the Metas
 });
 $(document).on("click", "#display-meta", function () {
   event.preventDefault();
-  $("input[name='meta']:checked").each(function() {
-    metaArray.push($(this).val());
+  
+  var chkArray=[];
+  
+  $(".chk:checked").each(function() {
+		chkArray.push($(this).val().toLowerCase());
+	});
+	
+	/* we join the array separated by the comma */
+  var selected;
+  var array1=[];
+  selected = chkArray.join(',') ;
+  console.log(chkArray);
+	if(selected.length > 0){
+    console.log("You have selected " + selected);	
+        array1=selected.split(",");
+        META1=array1[0];
+        META2=array1[1];
+       
+
+        console.log(META1,META2);
+    openconnection(DATA_ITEM,LIMIT);
+    $("#title").prop("checked",false);
+    $("#type").prop("checked",false);
+	}else{
+		alert("Please at least check one of the checkbox");	
+	}
+
 });
    
-  console.log("LIMIT:::"+ LIMIT);
-  console.log("META:   "+metaArray)
+ 
 
-  openconnection(DATA_ITEM,LIMIT,metaArray);
- metaArray.splice(0, metaArray.length);
-});
+
+//Animate the gifs from Still to Animate.
 $(document).on("click", ".foodgipsy", function () {
 
  var state= $(this).attr("data-state");
